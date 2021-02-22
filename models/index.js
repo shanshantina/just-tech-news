2fa60ad0cbb7dbbcc1fb600d1d232ddcf5566a8d
Shanshan Xu <tinaxu84@gmail.com>
2021-02-22 12:28:23 -0500

    post add

--- models/index.js
+++ models/index.js
@@ -1,5 +1,20 @@
 // collect all of the models
 
 const User = require('./User');
+const Post = require('./Post');
 
-module.exports = { User };
+
+// define the relationship between User and Post
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+
+// reverse association
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+
+module.exports = { User, Post };
